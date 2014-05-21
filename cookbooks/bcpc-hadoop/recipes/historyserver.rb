d071b59edc8be6b3a94e13c73c945a5c0d2cf144
bijugs <bnair@ll22-bcpc-r1n7.ll22.bcpc.bloomberg.com>
2014-05-21 17:53:34 -0400

    Changes to fix issues with history server UI

--- cookbooks/bcpc-hadoop/recipes/historyserver.rb
+++ cookbooks/bcpc-hadoop/recipes/historyserver.rb
@@ -5,6 +5,27 @@
   end
 end
 
+%w{/mr-history/tmp /mr-history/done /app-logs}.each do |d|
+  execute "create history server and applog directories" do
+    command "sudo -u hdfs hdfs dfs -mkdir -p #{d}  && sudo -u hdfs hdfs dfs -chown -R 1777 #{d}"
+  end
+end
+
+bash "chown history server directories" do
+  code <<-EOH
+   hdfs dfs -chown -R mapred:hadoop /mr-history 
+  EOH
+  user "hdfs"
+end
+
+bash "chown app log directory" do
+  code <<-EOH
+   hdfs dfs -chown yarn /app-logs
+  EOH
+  user "hdfs"
+end
+
+  
 service "hadoop-mapreduce-historyserver" do
   action [:enable, :start]
 end
