d133850275227835ceadcedb6035756668e8a5d8
Eric N. Vander Weele <ericvw@gmail.com>
2014-06-04 12:01:42 -0400

    build bcpc-bootstrap via Packer
    This change is to provide the means to build the initial bcpc-bootstrap
    machine image having similar configuration to that of a bare metal
    deployment.  The goal is have a functional machine image that mimics
    that exact same state after PXE booting a bare metal bcpc-bootstrap node
    and undergoing the typical installation procedure.

---
+++ images/packer/scripts/common/cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# delete all linux headers
+dpkg --list | awk '{print $2}' | grep linux-headers | xargs apt-get -y purge
+
+# does anyone use ppp anymore?
+apt-get -y purge ppp pppconfig pppoeconf
+
+# delete anonymous reporting of statistics
+apt-get -y purge popularity-contest
+
+# general clean-up and removal
+apt-get -y autoremove
+apt-get -y clean
+
+# remove Packer uploaded files
+rm -f VBoxGuestAdditions-*.iso
+rm .vbox_version
+
