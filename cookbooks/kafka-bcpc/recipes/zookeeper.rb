4b959654c44f1eb66910c6046b6a49d8ff99d3e4
Amit Anand <anandkamith@gmail.com>
2014-06-19 09:51:23 -0400

    Changes to set correct attributes and correct run order for recipes

--- cookbooks/kafka-bcpc/recipes/zookeeper.rb
+++ cookbooks/kafka-bcpc/recipes/zookeeper.rb
@@ -4,5 +4,7 @@
 
 file "#{node[:zookeeper][:data_dir]}/myid" do
   content "#{IPAddr.new(node[:ipaddress]).mask("0.0.0.255").to_i}\n"
+  owner node[:kafka][:user]
+  group node[:kafka][:group]
   action :create_if_missing
 end
