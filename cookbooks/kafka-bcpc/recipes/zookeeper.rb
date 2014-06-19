4a372101033ee7e13c4571d8a47aefb3b59d9b58
Amit Anand <anandkamith@gmail.com>
2014-06-19 12:03:36 -0400

    Added kafka::_setup recipe to zookeeper recipe

--- cookbooks/kafka-bcpc/recipes/zookeeper.rb
+++ cookbooks/kafka-bcpc/recipes/zookeeper.rb
@@ -2,6 +2,8 @@
 # Cookbook Name:: kafka-bcpc 
 # Recipe: Zookeeper
 
+include_recipe "kafka::_setup"
+
 file "#{node[:zookeeper][:data_dir]}/myid" do
   content "#{IPAddr.new(node[:ipaddress]).mask("0.0.0.255").to_i}\n"
   owner node[:kafka][:user]
