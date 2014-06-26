163f1feac5524444d3a83904e63b3bb86d3cbec2
Amit Anand <anandkamith@gmail.com>
2014-06-26 15:40:24 -0400

    Added multiple ZK hosts to connect to ZK

--- cookbooks/kafka-bcpc/recipes/kafka.rb
+++ cookbooks/kafka-bcpc/recipes/kafka.rb
@@ -15,7 +15,9 @@ ruby_block "kafkaup" do
   i = 0
   block do
     @brokerpath="/brokers/ids/#{node[:kafka][:broker_id]}"
-    @zk_host = "#{node[:kafka][:zookeeper][:connect][0]}:2181"
+    #@zk_host = "#{node[:kafka][:zookeeper][:connect][0]}:2181"
+    @zk_host = node[:kafka][:zookeeper][:connect].map{|zkh| "#{zkh}:2181"}.join(",")
+    Chef::Log.info("Zookeeper hosts are #{@zk_host}")
     while !znode_exists?(@brokerpath, @zk_host)
       kafka_in_zk = znode_exists?(@brokerpath, @zk_host)
       if !kafka_in_zk and i < 10
@@ -28,7 +30,7 @@ ruby_block "kafkaup" do
         Chef::Log.info("Broker #{@brokerpath} existance : #{znode_exists?(@brokerpath, @zk_host)}")
       end
     end
-    Chef::Log.info("Kafka is up and running.")
+    Chef::Log.info("Kafka with znode #{@brokerpath} is up and running.")
   end
   action :nothing
 end
