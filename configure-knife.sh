dda900a3dfd7a9b3d9f838f8d5011f0d6ffcc1ee
Chris Morgan <mihalis68@gmail.com>
2014-12-11 16:38:27 -0500

    clean up knife configuration. Use correct chef server URL

--- configure-knife.sh
+++ configure-knife.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+if [[ -z "$2" ]]; then
+    echo "Usage $0: <chef-server-url> <chef-server-password>"
+    exit 1
+fi
+CHEFSERVER="$1"
+CHEFPASSWD="$2"
 if [[ ! -d .chef ]]; then
   knife configure --initial <<EOF
 .chef/knife.rb
-http://10.0.100.3:4000
+${CHEFSERVER}
 
 
 /etc/chef-server/admin.pem
 
 /etc/chef-server/chef-validator.pem
 .
-welcome
+${CHEFPASSWD}
 EOF
 else
   echo "Knife configuration already exists!"
