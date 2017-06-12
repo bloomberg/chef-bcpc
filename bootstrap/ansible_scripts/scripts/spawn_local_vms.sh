a7352d7a0d29fd0e1e6ea4bb4b82a856fbfe9de1
David A. Desrosiers <setuid@gmail.com>
2017-06-12 12:46:15 -0400

    Lots of cleanup, removed unnecessary sed switching, code reduction, vbox_clean.sh rewrite

--- bootstrap/ansible_scripts/scripts/spawn_local_vms.sh
+++ bootstrap/ansible_scripts/scripts/spawn_local_vms.sh
@@ -13,9 +13,9 @@
 
 set -eu
 
-for CMD in VBoxManage git pcregrep; do
-  if ! which $CMD >/dev/null; then
-    echo "Could not find $CMD on PATH, exiting." >&2
+for binary in VBoxManage git; do
+  if ! [ -x "$(command -v $binary)" ]; then
+    echo "Error: Required program $binary not found in your PATH, please fix! Exiting on error. $?" >&2
     exit 1
   fi
 done
@@ -24,24 +24,18 @@ done
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 # load bootstrap configurations
-source $REPO_ROOT/bootstrap/config/bootstrap_config.sh.defaults
-CONFIG_OVERRIDES=$REPO_ROOT/bootstrap/config/bootstrap_config.sh.overrides
+source "$REPO_ROOT"/bootstrap/config/bootstrap_config.sh.defaults
+CONFIG_OVERRIDES="$REPO_ROOT"/bootstrap/config/bootstrap_config.sh.overrides
 
 if [[ -f $CONFIG_OVERRIDES ]]; then
-  source $CONFIG_OVERRIDES
+  source "$CONFIG_OVERRIDES"
 fi
 
-# get the location of the default machine folder
-if [[ $(uname) == "Darwin" ]]; then
-  SED="sed -E"
-else
-  SED="sed -r"
-fi
 DMF_SET=$(VBoxManage list systemproperties | grep '^Default machine folder:')
-OLD_DMF_PATH=$(echo "$DMF_SET" | $SED 's/^Default machine folder:[[:space:]]+(.+)$/\1/')
+OLD_DMF_PATH=$(echo "$DMF_SET" | sed -n 's/Default machine folder: *//p')
 DMF_PATH="${ANSIBLE_VM_DIR:-$OLD_DMF_PATH}"
 
-$REPO_ROOT/bootstrap/ansible_scripts/scripts/vbox_clean.sh
+"$REPO_ROOT"/bootstrap/ansible_scripts/scripts/vbox_clean.sh
 
 # destroy and recreate host-only networks
 VBoxManage hostonlyif remove vboxnet0 && true
@@ -64,12 +58,12 @@ VBoxManage dhcpserver remove --ifname vboxnet2 && true
 
 # Initialize VM lists
 VMS="ansible-bcpc-vm1 ansible-bcpc-vm2 ansible-bcpc-vm3"
-if [ $MONITORING_NODES -gt 0 ]; then
+if [[ $MONITORING_NODES -gt 0 ]]; then
   i=1
-  while [ $i -le $MONITORING_NODES ]; do
-    MON_VM="ansible-bcpc-vm`expr 3 + $i`"
+  while [[ $i -le $MONITORING_NODES ]]; do
+    MON_VM="ansible-bcpc-vm$((3 + i))"
     VMS="$VMS $MON_VM"
-    i=`expr $i + 1`
+    i=$((i + 1))
   done
 fi
 
@@ -77,8 +71,8 @@ fi
 echo "Creating bootstrap VM"
 VBoxManage createvm --basefolder "$DMF_PATH" --name ansible-bcpc-bootstrap --register
 VBoxManage modifyvm ansible-bcpc-bootstrap --ostype Ubuntu_64
-VBoxManage modifyvm ansible-bcpc-bootstrap --memory $BOOTSTRAP_VM_MEM
-VBoxManage modifyvm ansible-bcpc-bootstrap --cpus $BOOTSTRAP_VM_CPUS
+VBoxManage modifyvm ansible-bcpc-bootstrap --memory "$BOOTSTRAP_VM_MEM"
+VBoxManage modifyvm ansible-bcpc-bootstrap --cpus "$BOOTSTRAP_VM_CPUS"
 VBoxManage modifyvm ansible-bcpc-bootstrap --nic1 nat
 VBoxManage modifyvm ansible-bcpc-bootstrap --nictype1 82543GC
 VBoxManage modifyvm ansible-bcpc-bootstrap --nic2 hostonly
@@ -92,11 +86,11 @@ VBoxManage modifyvm ansible-bcpc-bootstrap --nictype4 82543GC
 VBoxManage modifyvm ansible-bcpc-bootstrap --hostonlyadapter4 vboxnet2
 # configure storage devices for bootstrap VM
 VBoxManage storagectl ansible-bcpc-bootstrap --name "SATA" --add sata --portcount 3 --hostiocache on
-VBoxManage createhd --filename "$DMF_PATH/ansible-bcpc-bootstrap/ansible-bcpc-bootstrap-sda.vdi" --size $BOOTSTRAP_VM_DRIVE_SIZE
+VBoxManage createhd --filename "$DMF_PATH/ansible-bcpc-bootstrap/ansible-bcpc-bootstrap-sda.vdi" --size "$BOOTSTRAP_VM_DRIVE_SIZE"
 VBoxManage storageattach ansible-bcpc-bootstrap --storagectl "SATA" --device 0 --port 0 --type hdd --medium "$DMF_PATH/ansible-bcpc-bootstrap/ansible-bcpc-bootstrap-sda.vdi"
 VBoxManage createhd --filename "$DMF_PATH/ansible-bcpc-bootstrap/ansible-bcpc-bootstrap-sdb.vdi" --size 8192
 VBoxManage storageattach ansible-bcpc-bootstrap --storagectl "SATA" --device 0 --port 1 --type hdd --medium "$DMF_PATH/ansible-bcpc-bootstrap/ansible-bcpc-bootstrap-sdb.vdi"
-VBoxManage createhd --filename "$DMF_PATH/ansible-bcpc-bootstrap/ansible-bcpc-bootstrap-sdc.vdi" --size $BOOTSTRAP_VM_DRIVE_SIZE
+VBoxManage createhd --filename "$DMF_PATH/ansible-bcpc-bootstrap/ansible-bcpc-bootstrap-sdc.vdi" --size "$BOOTSTRAP_VM_DRIVE_SIZE"
 VBoxManage storageattach ansible-bcpc-bootstrap --storagectl "SATA" --device 0 --port 2 --type hdd --medium "$DMF_PATH/ansible-bcpc-bootstrap/ansible-bcpc-bootstrap-sdc.vdi"
 # configure DVD drive to boot from and set boot device to DVD drive (user must attach boot medium)
 VBoxManage storagectl ansible-bcpc-bootstrap --name "IDE" --add ide
@@ -105,46 +99,46 @@ VBoxManage modifyvm ansible-bcpc-bootstrap --boot1 dvd
 
 # spawn and configure cluster VMs
 # note: NIC type 82543GC is required for PXE boot to work
-for VM in $VMS; do
-  echo "Creating cluster VM $VM"
-  VBoxManage createvm --basefolder "$DMF_PATH" --name $VM --register
-  VBoxManage modifyvm $VM --ostype Ubuntu_64
-  VBoxManage modifyvm $VM --memory $CLUSTER_VM_MEM
-  VBoxManage modifyvm $VM --cpus $CLUSTER_VM_CPUS
-  VBoxManage modifyvm $VM --nic1 hostonly
-  VBoxManage modifyvm $VM --nictype1 82543GC
-  VBoxManage modifyvm $VM --hostonlyadapter1 vboxnet0
-  VBoxManage modifyvm $VM --nic2 hostonly
-  VBoxManage modifyvm $VM --nictype2 82543GC
-  VBoxManage modifyvm $VM --hostonlyadapter2 vboxnet1
-  VBoxManage modifyvm $VM --nic3 hostonly
-  VBoxManage modifyvm $VM --nictype3 82543GC
-  VBoxManage modifyvm $VM --hostonlyadapter3 vboxnet2
-  VBoxManage storagectl $VM --name "SATA" --add sata --portcount 5 --hostiocache on
+for vm in $VMS; do
+  echo "Creating cluster VM $vm"
+  VBoxManage createvm --basefolder "$DMF_PATH" --name "$vm" --register
+  VBoxManage modifyvm "$vm" --ostype Ubuntu_64
+  VBoxManage modifyvm "$vm" --memory "$CLUSTER_VM_MEM"
+  VBoxManage modifyvm "$vm" --cpus "$CLUSTER_VM_CPUS"
+  VBoxManage modifyvm "$vm" --nic1 hostonly
+  VBoxManage modifyvm "$vm" --nictype1 82543GC
+  VBoxManage modifyvm "$vm" --hostonlyadapter1 vboxnet0
+  VBoxManage modifyvm "$vm" --nic2 hostonly
+  VBoxManage modifyvm "$vm" --nictype2 82543GC
+  VBoxManage modifyvm "$vm" --hostonlyadapter2 vboxnet1
+  VBoxManage modifyvm "$vm" --nic3 hostonly
+  VBoxManage modifyvm "$vm" --nictype3 82543GC
+  VBoxManage modifyvm "$vm" --hostonlyadapter3 vboxnet2
+  VBoxManage storagectl "$vm" --name "SATA" --add sata --portcount 5 --hostiocache on
 
   PORT=0
   for DISK in a b c d e; do
-      VBoxManage createhd --filename "$DMF_PATH/$VM/$VM-sd$DISK.vdi" --size $CLUSTER_VM_DRIVE_SIZE
-      VBoxManage storageattach $VM --storagectl "SATA" --device 0 --port $PORT --type hdd --medium "$DMF_PATH/$VM/$VM-sd$DISK.vdi"
+      VBoxManage createhd --filename "$DMF_PATH/$vm/$vm-sd$DISK.vdi" --size "$CLUSTER_VM_DRIVE_SIZE"
+      VBoxManage storageattach "$vm" --storagectl "SATA" --device 0 --port $PORT --type hdd --medium "$DMF_PATH/$vm/$vm-sd$DISK.vdi"
       PORT=$((PORT+1))
   done
 
   # attach PXE boot ROM
-  VBoxManage setextradata $VM VBoxInternal/Devices/pcbios/0/Config/LanBootRom "$BOOTSTRAP_CACHE_DIR/gpxe-1.0.1-80861004.rom"
+  VBoxManage setextradata "$vm" VBoxInternal/Devices/pcbios/0/Config/LanBootRom "$BOOTSTRAP_CACHE_DIR/gpxe-1.0.1-80861004.rom"
 done
 
 # common changes to execute on all VMs
 for VM in ansible-bcpc-bootstrap $VMS; do
-  VBoxManage modifyvm $VM --vram 16
-  VBoxManage modifyvm $VM --largepages on
-  VBoxManage modifyvm $VM --nestedpaging on
-  VBoxManage modifyvm $VM --vtxvpid on
-  VBoxManage modifyvm $VM --hwvirtex on
-  VBoxManage modifyvm $VM --ioapic on
-  VBoxManage modifyvm $VM --uart1 0x3F8 4
-  VBoxManage modifyvm $VM --uart2 0x2F8 3
-  VBoxManage modifyvm $VM --uartmode1 disconnected
-  VBoxManage modifyvm $VM --uartmode2 disconnected
+  VBoxManage modifyvm "$vm" --vram 16
+  VBoxManage modifyvm "$vm" --largepages on
+  VBoxManage modifyvm "$vm" --nestedpaging on
+  VBoxManage modifyvm "$vm" --vtxvpid on
+  VBoxManage modifyvm "$vm" --hwvirtex on
+  VBoxManage modifyvm "$vm" --ioapic on
+  VBoxManage modifyvm "$vm" --uart1 0x3F8 4
+  VBoxManage modifyvm "$vm" --uart2 0x2F8 3
+  VBoxManage modifyvm "$vm" --uartmode1 disconnected
+  VBoxManage modifyvm "$vm" --uartmode2 disconnected
 done
 
 # functions to act like associative arrays
@@ -187,15 +181,15 @@ cluster_name: Test-Laptop-Ansible
 nodes:
 YAML_HEAD
 
-for VM in ansible-bcpc-bootstrap $VMS; do
-  MAC_ADDRESS=$(VBoxManage showvminfo --machinereadable $VM | pcregrep -o1 -M '^hostonlyadapter\d="vboxnet0"$\n*^macaddress\d="(.+)"' | $SED 's/^(..)(..)(..)(..)(..)(..)$/\1:\2:\3:\4:\5:\6/')
+for vm in ansible-bcpc-bootstrap $VMS; do
+  MAC_ADDRESS=$(VBoxManage showvminfo --machinereadable "$vm" | grep macaddress1 |cut -d"\"" -f 2 | fold -w2 | paste -sd':' -)
   cat << EoF
-  $VM:
+  $vm:
     domain: bcpc.example.com
     hardware_type: Virtual
-    ip_address: $(get_node_ip $VM)
+    ip_address: $(get_node_ip "$vm")
     ipmi_address:
-    mac_address: $MAC_ADDRESS
-    role: $(get_node_role $VM)
+    mac_address: "$MAC_ADDRESS"
+    role: $(get_node_role "$vm")
 EoF
 done
