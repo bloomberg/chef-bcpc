16411d00c1903f74568d4be5a43f3ac9c1cdc806
Srinivas Sakhamuri <ssakhamuri@bloomberg.net>
2018-11-30 16:02:37 -0500

    Update tests

--- tests/test_nova.py
+++ tests/test_nova.py
@@ -2,24 +2,16 @@ import pytest
 
 @pytest.mark.headnodes
 @pytest.mark.parametrize("name", [
-    ("nova-api"),
-    ("nova-scheduler"),
-    ("nova-consoleauth"),
-    ("nova-conductor"),
-    ("nova-novncproxy"),
-    ("nova-scheduler"),
+    pytest.param("nova-api", marks=pytest.mark.headnodes),
+    pytest.param("nova-scheduler", marks=pytest.mark.headnodes),
+    pytest.param("nova-consoleauth", marks=pytest.mark.headnodes),
+    pytest.param("nova-conductor", marks=pytest.mark.headnodes),
+    pytest.param("nova-novncproxy", marks=pytest.mark.headnodes),
+    pytest.param("nova-scheduler", marks=pytest.mark.headnodes),
+    pytest.param("nova-compute", marks=pytest.mark.worknodes),
+    pytest.param("nova-api-metadata", marks=pytest.mark.worknodes),
 ])
 def test_services_head(host, name):
-    s = host.service(name)
-    assert s.is_running
-    assert s.is_enabled
-
-@pytest.mark.worknodes
-@pytest.mark.parametrize("name", [
-    ("nova-compute"),
-    ("nova-api-metadata"),
-])
-def test_services_work(host, name):
     s = host.service(name)
     assert s.is_running
     assert s.is_enabled
