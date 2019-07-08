04db222f12c20c8a28c46bc8ab9d88421dc6da0c
Justin J. Pacheco <justinjpacheco@gmail.com>
2019-07-08 12:59:18 -0400

    Update etcd.rb

--- chef/cookbooks/bcpc/attributes/etcd.rb
+++ chef/cookbooks/bcpc/attributes/etcd.rb
@@ -7,7 +7,7 @@ default['bcpc']['etcd']['remote']['file'] = etcd_file
 default['bcpc']['etcd']['remote']['source'] = "#{default['bcpc']['file_server']['url']}/#{etcd_file}"
 default['bcpc']['etcd']['remote']['checksum'] = '1620a59150ec0a0124a65540e23891243feb2d9a628092fb1edcc23974724a45'
 
-default['bcpc']['etcd']['ssl']['dir'] = '/etc/ssl/etcd'
+default['bcpc']['etcd']['ssl']['dir'] = '/etc/etcd/ssl'
 default['bcpc']['etcd']['ca']['crt']['filepath'] = "#{default['bcpc']['etcd']['ssl']['dir']}/ca.pem"
 default['bcpc']['etcd']['client-ro']['crt']['filepath'] = "#{default['bcpc']['etcd']['ssl']['dir']}/client-ro.pem"
 default['bcpc']['etcd']['client-ro']['key']['filepath'] = "#{default['bcpc']['etcd']['ssl']['dir']}/client-ro-key.pem"
