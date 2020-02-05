c052532e9cda2cdcbefcb721887983f02d44d035
Srinivas Sakhamuri <srini.openstack@gmail.com>
2020-02-04 20:06:24 -0500

    Update rally.rb

--- chef/cookbooks/bcpc/attributes/rally.rb
+++ chef/cookbooks/bcpc/attributes/rally.rb
@@ -3,7 +3,7 @@
 ###############################################################################
 
 default['bcpc']['rally']['enabled'] = true
-default['bcpc']['rally']['rally_openstack']['version'] = '1.5.0'
+default['bcpc']['rally']['rally_openstack']['version'] = '1.6.0'
 default['bcpc']['rally']['rally']['version'] = '1.6.0'
 default['bcpc']['rally']['ssl_verify'] = false
 default['bcpc']['rally']['conf_dir'] = '/etc/rally'
