ce25a700220a1e414b943395d02feb0d2f48f71a
Peter Lee <2292801+peter-at@users.noreply.github.com>
2020-02-06 17:33:54 -0500

    fix linting

--- virtual/lib/util.rb
+++ virtual/lib/util.rb
@@ -1,43 +1,42 @@
 module Util
-
-
-# get virtualbox private network suffix to use
-# * set to environment variable 'VBOX_NET_SUFFIX'
-# * default to a static hash
-# * eg: VBOX_NET_SUFFIX = ENV['VBOX_NET_SUFFIX'] || '_'+hash(__dir__)
-VBOX_NET_SUFFIX = begin 
-  if ENV.has_key? 'VBOX_NET_SUFFIX'
-    suffix = ENV['VBOX_NET_SUFFIX']
-  else
-    require 'digest'
-    seed = Digest::SHA1.hexdigest __dir__
-    rgen = Random::new(seed.hex)
-    space = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
-    hash = (0...5).map { space[rgen.rand(space.length)] }.join
-    suffix = '_' + hash
-  end
-  suffix
-end
-
-def Util.privnet_args(name, nic_type: '82543GC', macaddr: nil)
-  vbox_args = { }
-  vbox_args[:virtualbox__intnet] = name + VBOX_NET_SUFFIX
-  vbox_args[:nic_type] = nic_type
-  vbox_args[:auto_config] = false
-  if not macaddr.nil?
-    if not macaddr.scan(/\D/).empty?
-      raise "mac address to virtualbox should only be numbers (got #{macaddr})"
+  # initialize constants
+  # ## virtualbox private network suffix to use
+  # * set to environment variable 'VBOX_NET_SUFFIX'
+  # * default to a static hash
+  # * eg: VBOX_NET_SUFFIX = ENV['VBOX_NET_SUFFIX'] || '_'+hash(__dir__)
+  VBOX_NET_SUFFIX = begin
+    if ENV.key? 'VBOX_NET_SUFFIX'
+      suffix = ENV['VBOX_NET_SUFFIX']
+    else
+      require 'digest'
+      seed = Digest::SHA1.hexdigest __dir__
+      rgen = Random.new(seed.hex)
+      space = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
+      hash = (0...5).map { space[rgen.rand(space.length)] }.join
+      suffix = '_' + hash
     end
-    vbox_args[:mac] = macaddr
+    suffix
   end
-  return vbox_args
-end
 
-def Util.mount_apt_cache(config)
-  user_data_path = Vagrant.user_data_path.to_s
-  cache_dir = File.join(user_data_path, 'cache', 'apt', config.vm.box)
-  apt_cache_dir = '/var/cache/apt/archives'
-  config.vm.synced_folder cache_dir, apt_cache_dir, create: true, owner: '_apt'
-end
+  def self.privnet_args(name, nic_type: '82543GC', macaddr: nil)
+    vbox_args = {}
+    vbox_args[:virtualbox__intnet] = name + VBOX_NET_SUFFIX
+    vbox_args[:nic_type] = nic_type
+    vbox_args[:auto_config] = false
+    unless macaddr.nil?
+      unless macaddr.scan(/\D/).empty?
+        raise "mac addr to virtualbox should only be numbers (got #{macaddr})"
+      end
+      vbox_args[:mac] = macaddr
+    end
+    vbox_args
+  end
 
+  def self.mount_apt_cache(config)
+    user_data_path = Vagrant.user_data_path.to_s
+    cache_dir = File.join(user_data_path, 'cache', 'apt', config.vm.box)
+    apt_cache_dir = '/var/cache/apt/archives'
+    config.vm.synced_folder cache_dir, apt_cache_dir,
+        create: true, owner: '_apt'
+  end
 end
