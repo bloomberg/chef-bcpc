7c50fdbec49f6187cc3194313dbb94db366f826f
Peter Lee <2292801+peter-at@users.noreply.github.com>
2020-02-07 17:40:19 -0500

    use bool for enablement of vbox name suffix

--- virtual/lib/util.rb
+++ virtual/lib/util.rb
@@ -1,30 +1,25 @@
 module Util
   # initialize constants
   # ## virtualbox private network suffix to use
-  # * set to environment variable 'VBOX_NET_SUFFIX'
-  # * default to a static hash
-  # * eg: VBOX_NET_SUFFIX = ENV['VBOX_NET_SUFFIX'] || '_'+hash(__dir__)
-  VBOX_NET_SUFFIX = begin
-    if ENV.key? 'VBOX_NET_SUFFIX'
-      suffix = ENV['VBOX_NET_SUFFIX']
-    else
+  # * control by environment variable 'ENABLE_VBOX_SUFFIX'
+  # * set and non-empty -> set to a static hash base off full path of this file
+  # * not set or empty -> set to empty (as if there's no suffix)
+  VBOX_SUFFIX = begin
+    if ENV.key?('ENABLE_VBOX_SUFFIX') && !ENV['ENABLE_VBOX_SUFFIX'].empty?
       require 'digest'
       seed = Digest::SHA1.hexdigest __dir__
       rgen = Random.new(seed.hex)
       space = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
       hash = (0...5).map { space[rgen.rand(space.length)] }.join
       suffix = '_' + hash
+    else
+      suffix = ''
     end
     suffix
   end
 
-  def self.privnet_args(name, nic_type: '82543GC', macaddr: nil)
-    vbox_args = {}
-    vbox_args[:virtualbox__intnet] = name + VBOX_NET_SUFFIX
-    vbox_args[:nic_type] = nic_type
-    vbox_args[:auto_config] = false
-    vbox_args[:mac] = macaddr unless macaddr.nil?
-    vbox_args
+  def self.vbox_name(name)
+    name + VBOX_SUFFIX
   end
 
   def self.mount_apt_cache(config)
