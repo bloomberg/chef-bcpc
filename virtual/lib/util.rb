2d04295357189a6b9e4ba891e1cd29115bb61b45
Peter Lee <2292801+peter-at@users.noreply.github.com>
2020-02-10 09:10:48 -0500

    update comment and make it pass linter

--- virtual/lib/util.rb
+++ virtual/lib/util.rb
@@ -1,35 +1,15 @@
 module Util
-  # initialize constants
-  # ## virtualbox private network suffix to use
-  # * control by environment variable 'ENABLE_VBOX_SUFFIX'
-  # * set and non-empty -> set to a static hash base off full path of this file
-  # * not set or empty -> set to empty (as if there's no suffix)
-def self.vbox_name(name:) 
-  # return name if suffix not enabled
-  unless ENV.key?('ENABLE_VBOX_SUFFIX')
-    return name
-  end
-
-  # return name + hashed __dir__
-  require 'digest/sha1'
-  hash = Digest::SHA1.hexdigest(__dir__)[0,7]
-  return name + '_' + hash
-end
-    if ENV.key?('ENABLE_VBOX_SUFFIX') && !ENV['ENABLE_VBOX_SUFFIX'].empty?
-      require 'digest'
-      seed = Digest::SHA1.hexdigest __dir__
-      rgen = Random.new(seed.hex)
-      space = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
-      hash = (0...5).map { space[rgen.rand(space.length)] }.join
-      suffix = '_' + hash
-    else
-      suffix = ''
-    end
-    suffix
-  end
-
+  # returns vbox_name with or without suffix base on set vs unset
+  # of the environment var 'ENABLE_VBOX_SUFFIX'
   def self.vbox_name(name)
-    name + VBOX_SUFFIX
+    # return name if suffix not enabled
+    unless ENV.key?('ENABLE_VBOX_SUFFIX')
+      return name
+    end
+    # return name + hashed __dir__
+    require 'digest/sha1'
+    hash = Digest::SHA1.hexdigest(__dir__)[0, 7]
+    name + '_' + hash
   end
 
   def self.mount_apt_cache(config)
