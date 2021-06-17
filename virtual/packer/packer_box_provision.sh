bloomberg/chef-bcpc

commit 2fd31b18a319897c8d4a7679c7b5d8f35737751c

packer: Install tools mated with kernel
Calico requires linux-tools-`uname -r` to be present.  With
upstream builds, it may be the case that a packer image is
generated on a specific date, with kernel x.y.z installed.
The Ansible automation will install the linux-tools-*
metapackage, which may install kernel x.y.theta tools.

To avoid this drift, install the tools mated with the
kernel at packer provisioning time.

Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

virtual/packer/packer_box_provision.sh
@@ -65,7 +65,7 @@ function upgrade_system {
 
 function configure_linux_kernel {
     if [ -n "${kernel_version}" ]; then
-        apt-get install -y "linux-${kernel_version}"
+        apt-get install -y "linux-${kernel_version} linux-tools-${kernel_version}"
     fi
 
     # Disable IPv6

commit 2a5ec634dedb09ec0deab89ff34454c6f202901f

packer; Fix breakage introduced in #1860
Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

virtual/packer/packer_box_provision.sh
@@ -65,7 +65,7 @@ function upgrade_system {
 
 function configure_linux_kernel {
     if [ -n "${kernel_version}" ]; then
-        apt-get install -y "linux-${kernel_version} linux-tools-${kernel_version}"
+        apt-get install -y "linux-${kernel_version}" "linux-tools-${kernel_version}"
     fi
 
     # Disable IPv6