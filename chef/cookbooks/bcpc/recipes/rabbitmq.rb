bloomberg/chef-bcpc

commit 1482db1e923c9198324f2e2fee3241efb86f623d

rabbitmq: Do not restart the broker late in build
The RabbitMQ broker is configured to not automatically
sync the queues when a broker leaves the cluster at
present. Yet, Chef had multiple deferred reboots and
it was kicking the broker at the end of the client
run.

This results 'weird' corruption/behavior in some versions
of RabbitMQ, and we were probably just flat out lucky
that this was formerly working with 3.8.x for so long.

Do not restart the broker late in the build as we do not
synchronize the queues after!

Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

chef/cookbooks/bcpc/recipes/rabbitmq.rb
@@ -56,24 +56,24 @@ end
 
 template '/etc/rabbitmq/rabbitmq.conf.d/bcpc.conf' do
   source 'rabbitmq/bcpc.conf.erb'
-  notifies :restart, 'service[rabbitmq-server]', :delayed
+  notifies :restart, 'service[rabbitmq-server]', :immediately
 end
 
 template '/etc/default/rabbitmq-server' do
   source 'rabbitmq/default.erb'
-  notifies :restart, 'service[rabbitmq-server]', :delayed
+  notifies :restart, 'service[rabbitmq-server]', :immediately
 end
 
 file '/var/lib/rabbitmq/.erlang.cookie' do
   mode '400'
   content config['rabbit']['cookie']
-  notifies :restart, 'service[rabbitmq-server]', :delayed
+  notifies :restart, 'service[rabbitmq-server]', :immediately
 end
 
 execute 'enable rabbitmq web mgmt' do
   command '/usr/sbin/rabbitmq-plugins enable rabbitmq_management'
   not_if '/usr/sbin/rabbitmq-plugins list -m -e | grep "^rabbitmq_management$"'
-  notifies :restart, 'service[rabbitmq-server]', :delayed
+  notifies :restart, 'service[rabbitmq-server]', :immediately
 end
 
 template '/etc/rabbitmq/rabbitmq.config' do

commit 6729b3a971a8c92bda7c34e8630f9f346958a040

rabbitmq: Fix Focal breakage introduced in #2097
The API path used was only supported in RabbitMQ 3.9; we
need to support RabbitMQ 3.8 (conditionally) as well. 3.9
ships with Jammy, and 3.8 is used with Focal and below.

Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

chef/cookbooks/bcpc/recipes/rabbitmq.rb
@@ -93,6 +93,11 @@ begin
     end
 
     hosts = hosts.join(' ')
+    healthcheck_path = if ['18.04', '20.04'].include?(node['platform_version'])
+                         '/api/healthchecks/node'
+                       else
+                         '/api/health/checks/alarms'
+                       end
 
     bash 'join rabbitmq cluster' do
       code <<-DOC
@@ -105,7 +110,7 @@ begin
         member=''
 
         for h in #{hosts}; do
-          status=$(curl -su '#{username}:#{password}' "http://${h}:55672/api/health/checks/alarms")
+          status=$(curl -su '#{username}:#{password}' "http://${h}:55672#{healthcheck_path}")
           if echo ${status} | jq -e 'select(.status == "ok")'; then
             member=${h}
             break