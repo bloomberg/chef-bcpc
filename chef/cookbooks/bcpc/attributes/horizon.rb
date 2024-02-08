717e27c86acabce2d82da21274dff4f84e646a1e
Zhan Zhang <zzhang953@bloomberg.net>
2024-02-08 09:13:48 -0500

    Parameterize horizon's WSGIDaemon processes and threads
    Signed-off-by: Zhan Zhang <zzhang953@bloomberg.net>

---
+++ chef/cookbooks/bcpc/attributes/horizon.rb
@@ -0,0 +1,7 @@
+###############################################################################
+# horizon
+###############################################################################
+
+# Configs for Horizon Dashboard's WSGIDaemonProcess
+default['bcpc']['horizon']['workers'] = 3
+default['bcpc']['horizon']['worker_threads'] = 10
